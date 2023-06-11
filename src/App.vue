906201d2f0cdc7c1409f40e4e03e695b75a11f3e
Adela Matouskova <adela.matouskova@scssoft.com>
2023-06-11 17:06:47 +0200

    Buttons and gradients

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ButtonBase from '@/ButtonBase.vue'
+</script>
 
 <template>
   <header>
     <h1 class="text-3xl font-bold text-blue underline">Hello world!</h1>
-    <button class="gradient-blue">Hey there</button>
-    <button class="gradient-orange">Hey there</button>
+    <ButtonBase text="Hey there" />
+    <ButtonBase text="Hey there" type="secondary" />
+    <ButtonBase text="Hey there" type="tertiary" />
   </header>
 </template>
---
+++ src/ButtonBase.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const vProps = withDefaults(
+  defineProps<{
+    text: string
+    type?: 'primary' | 'secondary' | 'tertiary'
+  }>(),
+  {
+    type: 'primary'
+  }
+)
+
+defineEmits<{
+  (event: 'click'): void
+}>()
+
+const cClass = computed(() => {
+  switch (vProps.type) {
+    case 'primary':
+    default:
+      return 'bg-dark-blue hover:bg-blue'
+    case 'secondary':
+      return 'bg-gradient-orange hover:bg-gradient-orange-light'
+    case 'tertiary':
+      return 'bg-gradient-blue hover:bg-gradient-blue-light'
+  }
+})
+</script>
+
+<template>
+  <button :class="cClass">
+    {{ text }}
+  </button>
+</template>
