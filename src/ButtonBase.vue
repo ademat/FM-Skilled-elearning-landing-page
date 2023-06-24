b1cc750fde97cc9a07ca3a69148e490cbf1c359e
Adela Matouskova <adela.matouskova@scssoft.com>
2023-06-24 17:12:00 +0200

    Styled buttons

--- src/ButtonBase.vue
+++ src/ButtonBase.vue
@@ -19,17 +19,17 @@ const cClass = computed(() => {
   switch (vProps.type) {
     case 'primary':
     default:
-      return 'bg-dark-blue hover:bg-blue'
+      return 'bg-dark-blue hover:bg-blue py-[0.875rem] rounded-[1.96875rem]'
     case 'secondary':
-      return 'bg-gradient-orange hover:bg-gradient-orange-light'
+      return 'bg-gradient-blue hover:bg-gradient-blue-light py-[0.875rem] rounded-[1.75rem]'
     case 'tertiary':
-      return 'bg-gradient-blue hover:bg-gradient-blue-light'
+      return 'bg-gradient-orange hover:bg-gradient-orange-light py-4 rounded-[1.75rem]'
   }
 })
 </script>
 
 <template>
-  <button :class="cClass">
+  <button :class="cClass" class="px-8 text-white">
     {{ text }}
   </button>
 </template>
